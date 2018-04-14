24eaf63931b62e3ef67825d684ea92ffc905190a
Sarthak-Sureka <sarthaksureka97@gmail.com>
2018-04-14 07:19:23 +0000

    Added FeedResponseService

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,26 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getfeedresponse(String query){
+        FeedResponse feedResponse = new FeedResponse();
+        try{
+            WSRequest feedRequest = WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q",query)
+                    .setQueryParameter("output","rss")
+                    .get();
+            Document response = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item = response.getFirstChild().getFirstChild().getChildNodes().item(10);
+            feedResponse.title = item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            //item = 0 will return the whole item node in which the title node is the first child and
+            // if it would have contained sub nodes then that would be starting from second node
+            feedResponse.description = item.getChildNodes().item(4).getFirstChild().getNodeValue();
+            feedResponse.pubDate = item.getChildNodes().item(3).getFirstChild().getNodeValue();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return feedResponse;
+    }
 
 }
