cd932ba0c73e96cb22893ee74ab32dec5abf20c3
Sarthak-Sureka <sarthaksureka97@gmail.com>
2018-04-14 12:12:28 +0000

    Basic Layout Done

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,22 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Hi, what can i do for you??'
+		},
+        {
+            'sender': 'USER',
+            'text': 'How are you??'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'I am fine.'
+		}
+	];
 });
